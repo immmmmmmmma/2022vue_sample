9d84e6bb9adc4936bac1d41a19828a841e0a7ef3
immmmmmmmma <icanasd@gmail.com>
2022-10-07 12:01:23 +0900

    test

---
+++ src/components/MainVisual.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="visual"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    .visual{
+        height: 300px;
+        background: pink;
+    }
+</style>
